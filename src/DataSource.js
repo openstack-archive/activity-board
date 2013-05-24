dcafcf90abac46ac327c999b28f47fc3c8095ffc
Daniel Izquierdo <dizquierdo@bitergia.com>
2013-05-24 11:09:00 +0200

    fixed issue https://bugs.launchpad.net/openstack-community/+bug/1181028

--- src/DataSource.js
+++ src/DataSource.js
@@ -637,8 +637,8 @@ function DataSource(name, basic_metrics) {
 
         $.each(sorted, function(id, item) {
             if (scm_and_its && (!(Report.getReposMap()[item]))) return;
-            list += "<div class='subreport-list' id='"+item+"-nav'>";
-            list += "<div style='float:left;'>";
+            list += "<div class='subreport-list' id='"+item+"-nav' style='height:175px'>";
+            list += "<div>";
             if (report === "companies") 
                 list += "<a href='company.html?company="+item+"'>";
             else if (report === "repos") {
@@ -673,10 +673,12 @@ function DataSource(name, basic_metrics) {
             list += "</strong> +info</a>";
             list += "<br><a href='#nav'>^</a>";
             list += "</div>";
+            list += "<div style='height:150px'>";
             $.each(metrics, function(id, metric) {
                 list += "<div id='"+item+"-"+metric+"'";
                 list +=" class='subreport-list-item'></div>";
             });
+            list += "</div>";
             list += "</div>";
         });
         $("#"+div_id).append(list);
